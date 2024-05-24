854420cb126652a0c53941efe9bda4daf09168bb
galeanoyapio <166860602+galeanoyapio@users.noreply.github.com>
2024-05-24 17:25:17 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,62 @@
-#include "libdecoycontador.h"
 #include "MKL25Z4.h"
+
+int est=1;
+int decoUnidades[4]={1,2,3,4};
+int decoDecenas[4]={5,6,7,8};
+int cont[9][4]={
+    {0,0,0,0},
+    {0,0,0,1},       
+    {0,0,1,0},
+    {0,0,1,1},
+    {0,1,0,0},
+    {0,1,0,1},
+    {0,1,1,0},
+    {0,1,1,1},
+    {1,0,0,0},
+    {1,0,0,1}
+    };
+                int contB=0;
+
 int main(){
     SIM -> SCGC5 |= SIM_SCGC5_PORTA_MASK;
-    keyboardInit('b');
-    int salidas[7];
-    for(int i=0;i<7;i++){
-        PORTA->PCR[i]|=PORT_PCR_MUX(1);
+    est=PTA->PDIR&(1u<<5);
+    PORTA->PCR[est]|=PORT_PCR_MUX(0);
+    for(int q=0;q<4;q++){
+        PORTA->PCR[(decoUnidades[q])]|=PORT_PCR_MUX(1);
+        PORTA->PCR[(decoDecenas[q])]|=PORT_PCR_MUX(1);
     }
 
     while(1){
-        char tecla = keyboardRead('a');
-        deco7seg(tecla);
-
-
+        if(est==0){ //cuenta para delante
+            for(int i=0;i<10;i++){ //filas X  //se repite mientras que i sea menor a 10
+                for(int j=0;j<4;j++){ //columnas Y o Pines
+                                    //y  x
+                    PTA->PSOR|=((cont[i][j])<<(decoUnidades[j])); //la posicion del cont
+                }                                                 //remplaza al decounidades en el eje x 
+            if(i==9){
+                contB++;
+                for(int j=0;j<4;j++){ //columnas Y o Pines
+                                    //y  x
+                    PTA->PSOR|=((cont[contB][j])<<(decoUnidades[j])); //la posicion del cont
+                }      
+                i=0;                                        
+            }
+            }
+        }else{
+           for(int i=10;i>0;i--){ //filas X
+                for(int j=4;j>0;j--){ //columnas Y o Pines
+                                    //y  x
+                    PTA->PSOR|=((cont[i][j])<<(decoUnidades[j])); //la posicion del cont
+                }                                                 //remplaza al decounidades en el eje x 
+            if(i==0){
+                contB--;
+                for(int j=4;j>0;j--){ //columnas Y o Pines
+                                    //y  x
+                    PTA->PSOR|=((cont[contB][j])<<(decoUnidades[j])); //la posicion del cont
+                }      
+                i=9;                                        
+            }
+            }
+        }
     }
 }
